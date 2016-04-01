7b5277ef269b4163cbed47c23e01e6a5d4bf3c7f
navalkashyap <navalk@uw.edu>
2016-04-01 13:48:58 -0700

    Adding Struct

--- main.cpp
+++ main.cpp
@@ -18,9 +18,19 @@ using namespace std;
 /*
  * 
  */
-int main(int argc, char** argv) {
-    cout<<"Hello World!\nThis is a Linked List workshop"<<endl;
-    cout<<"Because I am Happy!!!";
-    return 0;
-}
+struct node {
+  int x;
+  node *next;
+};
 
+int main()
+{
+  node *root;      // This will be the unchanging first node
+
+  root = new node; // Now root points to a node struct
+  root->next = 0;  // The node root points to has its next pointer
+                   //  set equal to a null pointer
+  root->x = 5;     // By using the -> operator, you can modify the node
+                   //  a pointer (root in this case) points to.
+  cout<< root->x<<endl;
+}
