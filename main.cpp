2133ca1e41b9dc92b9e2d8802b94aa55e5faacea
navalkashyap <navalk@uw.edu>
2016-04-01 13:35:44 -0700

    New

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: naval
+ *
+ * Created on April 1, 2016, 1:29 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout<<"Hello World!"<<endl;
+    cout<<"Because I am Happy!!!";
+    return 0;
+}
+
